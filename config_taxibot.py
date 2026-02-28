ad809e6ea0c78bb096551169c0a272809e13fd85
KIRRuser <148910826+KIRRuser@users.noreply.github.com>
2026-02-28 21:09:19 +0300

    Update config_taxibot.py

--- config_taxibot.py
+++ config_taxibot.py
@@ -1,4 +1,4 @@
-token = '6473499327:AAHisAxt2hgwizxj8ulVBcc57pOw4JEJIDE'
+token = 'enter'
 id_group = '-1002031313595'
 
 import random
@@ -9,4 +9,5 @@ def generate_key():
     for i in range(16):
         key += random.choice(chars)
     return key
+
 
